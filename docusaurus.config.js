53a8c45f2b37015301b8f9f8e3338ffcd24e7fba
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-07-10 19:42:29 +1000

    fix dark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,6 +68,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'Master Scroll',
         logo: {
